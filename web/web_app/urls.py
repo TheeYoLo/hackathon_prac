c56c7de59a4ff21d3398f7367d79baa445f66f15
TheeYoLo <officialtheeyolo@gmail.com>
2026-04-08 19:54:52 +0530

    Implement user authentication with sign up and sign in functionality

--- web/web_app/urls.py
+++ web/web_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home_view, signin_view, signup_view
 
 urlpatterns = [
-    path('',home,name='home'),
+    path('', home_view, name='home'),
+    path('signup/', signup_view, name='signup'),
+    path('signin/', signin_view, name='signin'),
     ]
